096dc7e5a62825666158d220f2e2dfe799ea438b
jptrosclair <none@none>
2011-08-07 12:55:29 +0000

    Attempted source tree fix (no code/feature changes)
    --HG--
    extra : convert_revision : svn%3A7523448a-a8a0-4aac-83b0-0615d70583c7%408114

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("System.Net.FtpClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("FTP Client Library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("System.Net.FtpClient")]
