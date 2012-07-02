b92e7fa4b587f11172d8f9de5609d4e55da38833
jp@ITDEV-THINK.jude.lan <jp@ITDEV-THINK.jude.lan>
2012-07-02 11:08:04 -0500

    Added new unit tests project for testing code. Requires a ftp server to test against, server and credentials can be set in Config.cs of tests project.

---
+++ Tests/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace System.Net.FtpClient.Tests {
+    public class Config {
+        /// <summary>
+        /// The test upload all of the files in the FtpSourceCode directory as
+        /// part of the test procedures
+        /// </summary>
+        public static readonly string FtpSourceCode = @"..\..\..\..\";
+        public static readonly string FtpServer = "localhost";
+        public static readonly int FtpPort = 21;
+        public static readonly string FtpUser = "ftptest";
+        public static readonly string FtpPass = "ftptest";
+
+        public static System.IO.FileStream OpenTransactionLog() {
+            StackTrace trace = new StackTrace();
+
+            return new IO.FileStream(string.Format("{0}-TransactionLog.txt", trace.GetFrame(1).GetMethod().Name), 
+                IO.FileMode.OpenOrCreate, IO.FileAccess.Write);
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Net.FtpClient.Tests {
+    [TestClass]
+    public class Tests {
+        [TestMethod]
+        public void RunTests() {
+            UploadFiles();
+            DownloadFiles();
+            DeleteFiles();
+        }
+
+        void UploadFiles() {
+            using (FtpClient cl = new FtpClient(Config.FtpUser, Config.FtpPass, Config.FtpServer, Config.FtpPort)) {
+                cl.FtpLogStream = Config.OpenTransactionLog();
+                //cl.SslMode = FtpSslMode.None;
+                cl.TransferProgress += new FtpTransferProgress(OnTransferProgress);
+                cl.InvalidCertificate += new FtpInvalidCertificate(OnInvalidCertificate);
+                UploadDirectory(cl, Config.FtpSourceCode, "/");
+            }
+        }
+
+        void UploadDirectory(FtpClient cl, string path, string remotepath) {
+            if (!cl.DirectoryExists(remotepath)) {
+                cl.CreateDirectory(remotepath);
+            }
+
+            foreach (string f in System.IO.Directory.GetFiles(path, "*.cs")) {
+                string remote = string.Format("{0}/{1}", remotepath, System.IO.Path.GetFileName(f));
+
+                if (cl.FileExists(remote)) {
+                    cl.RemoveFile(remote);
+                }
+
+                cl.Upload(f, remote);
+            }
+
+            foreach (string d in System.IO.Directory.GetDirectories(path)) {
+                if (System.IO.Path.GetDirectoryName(d) != "Tests") {
+                    UploadDirectory(cl, d, string.Format("{0}/{1}", remotepath, System.IO.Path.GetFileName(d)));
+                }
+            }
+        }
+
+        void DownloadFiles() {
+            string root = @"C:\FTPTest";
+
+            using (FtpClient cl = new FtpClient(Config.FtpUser, Config.FtpPass, Config.FtpServer, Config.FtpPort)) {
+                cl.FtpLogStream = Config.OpenTransactionLog();
+                cl.TransferProgress += new FtpTransferProgress(OnTransferProgress);
+                cl.InvalidCertificate += new FtpInvalidCertificate(OnInvalidCertificate);
+                DownloadDirectory(cl, root, cl.CurrentDirectory);
+            }
+
+            System.IO.Directory.Delete(root, true);
+        }
+
+        void DownloadDirectory(FtpClient cl, string root, FtpDirectory path) {
+            if (!System.IO.Directory.Exists(root)) {
+                System.IO.Directory.CreateDirectory(root);
+            }
+
+            foreach (FtpFile f in path.Files.ToArray()) {
+                string local = string.Format(@"{0}\{1}", root, f.Name);
+
+                if (System.IO.File.Exists(local)) {
+                    System.IO.File.Delete(local);
+                }
+
+                cl.Download(f.FullName, local);
+            }
+
+            foreach (FtpDirectory d in path.Directories.ToArray()) {
+                DownloadDirectory(cl, string.Format(@"{0}\{1}", root, d.Name), d);
+            }
+        }
+
+        void DeleteFiles() {
+            using (FtpClient cl = new FtpClient(Config.FtpUser, Config.FtpPass, Config.FtpServer, Config.FtpPort)) {
+                cl.FtpLogStream = Config.OpenTransactionLog();
+                cl.TransferProgress += new FtpTransferProgress(OnTransferProgress);
+                cl.InvalidCertificate += new FtpInvalidCertificate(OnInvalidCertificate);
+                DeleteFiles(cl.CurrentDirectory);
+            }
+        }
+
+        void DeleteFiles(FtpDirectory path) {
+            foreach (FtpFile f in path.Files.ToArray()) {
+                f.Delete();
+            }
+
+            foreach (FtpDirectory d in path.Directories.ToArray()) {
+                DeleteFiles(d);
+                d.Delete();
+            }
+        }
+
+        void OnInvalidCertificate(FtpChannel c, InvalidCertificateInfo e) {
+            e.Ignore = true;
+        }
+
+        void OnTransferProgress(FtpTransferInfo e) {
+            if (e.TransferType == FtpTransferType.Download) {
+                Console.Write("Download: ");
+            }
+            else {
+                Console.Write("Upload: ");
+            }
+
+            Console.WriteLine("{0} {1}/{2} {3}%",
+                e.FileName, e.Transferred, e.Length, e.Percentage);
+        }
+    }
+}
