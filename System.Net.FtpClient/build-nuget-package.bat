72862be7fe0b3ac0959484919aef6a8b09ddfc76
jptrosclair <jptrosclair@gmail.com>
2013-04-25 10:18:38 -0500

    Modification to package building script so that it puts packages in a subdirectory 'packages'

--- System.Net.FtpClient/build-nuget-package.bat
+++ System.Net.FtpClient/build-nuget-package.bat
@@ -1,3 +1,4 @@
 @echo off
-nuget pack System.Net.FtpClient.csproj -Prop Configuration=Release
+if not exist "packages" md packages
+nuget pack System.Net.FtpClient.csproj -Prop Configuration=Release -OutputDirectory packages
 pause
