79a239d4c71f60aebecd99250f40bb8c9bd75a2b
jptrosclair <jptrosclair@gmail.com>
2014-02-17 15:54:40 -0600

    Fixed bug with manually specifying text encoding. If ASCII encoding is used (default value) and the server advertises UTF8 support then the encoding is automatically changed to UTF8 which is backwards compatible with ASCII. If you specify any other encoding besides ASCII it will be used regardless.

--- System.Net.FtpClient/FtpClient.cs
+++ System.Net.FtpClient/FtpClient.cs
@@ -194,8 +194,6 @@ namespace System.Net.FtpClient {
             }
         }
 
-        Encoding m_overrideEncoding = null;
-
         Encoding m_textEncoding = Encoding.ASCII;
         /// <summary>
         /// Gets or sets the text encoding being used when talking with the server. The default
@@ -211,12 +209,13 @@ namespace System.Net.FtpClient {
                 return m_textEncoding;
             }
             set {
+                if (value == null) {
+                    throw new InvalidOperationException("The Encoding property cannot be set to null.");
+                }
+
                 try {
                     m_lock.WaitOne();
-
-                    if (IsConnected)
-                        m_textEncoding = value;
-                    m_overrideEncoding = value;
+                    m_textEncoding = value;
                 }
                 finally {
                     m_lock.ReleaseMutex();
@@ -776,11 +775,6 @@ namespace System.Net.FtpClient {
                 if (Credentials == null)
                     throw new FtpException("No credentials have been specified");
 
-                if (m_overrideEncoding == null)
-                    m_textEncoding = Encoding.ASCII;
-                else
-                    m_textEncoding = m_overrideEncoding;
-
                 if (!IsClone) {
                     m_caps = FtpCapability.NONE;
                 }
@@ -833,15 +827,13 @@ namespace System.Net.FtpClient {
                     }
                 }
 
-                // Enable UTF8 if it's available
-                if (HasFeature(FtpCapability.UTF8)) {
+                // Enable UTF8 if the encoding is ASCII and UTF8 is supported
+                if (m_textEncoding == Encoding.ASCII && HasFeature(FtpCapability.UTF8)) {
                     // If the server supports UTF8 it should already be enabled and this
                     // command should not matter however there are conflicting drafts
                     // about this so we'll just execute it to be safe. 
-                    if (m_overrideEncoding == null || m_overrideEncoding == Encoding.UTF8) {
-                        Execute("OPTS UTF8 ON");
-                        m_textEncoding = Encoding.UTF8;
-                    }
+                    Execute("OPTS UTF8 ON");
+                    m_textEncoding = Encoding.UTF8;
                 }
 
                 FtpTrace.WriteLine("Text encoding: " + m_textEncoding.ToString());
