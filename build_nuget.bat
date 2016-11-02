a1fee6143db25f074898b0ffe706240ee0385446
Harsh Gupta <harshgupt1@gmail.com>
2016-11-02 12:55:24 +0530

    nuget release

---
+++ build_nuget.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set msbuild=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+set nuget=C:\Tools\Nuget\nuget.exe
+
+rd /q /s FluentFTP\bin
+
+:: Build dll without strong name
+%msbuild% /p:Configuration=Release FluentFTP\FluentFTP.csproj
+
+:: Build signed DLL
+rem %msbuild% /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile="%snk%" FluentFTP\FluentFTP.csproj
+
+%nuget% pack FluentFTP\FluentFTP.csproj -Prop Configuration=Release -OutputDirectory "Q:\Code\CS-OSS\FluentFTP-Nuget"
+pause
