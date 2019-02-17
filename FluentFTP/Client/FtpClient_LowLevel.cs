d97bbe23276c55f943d718551220b7ecf97a3a45
n0ix <n0ix@protonmail.com>
2019-02-17 17:47:03 +0100

    Added workaround if a server advertises a non-routeable ip in PASV Mode

--- FluentFTP/Client/FtpClient_LowLevel.cs
+++ FluentFTP/Client/FtpClient_LowLevel.cs
@@ -131,6 +131,13 @@ namespace FluentFTP {
 					host = (m.Groups["quad1"].Value + "." + m.Groups["quad2"].Value + "." + m.Groups["quad3"].Value + "." + m.Groups["quad4"].Value);
 
 				port = (int.Parse(m.Groups["port1"].Value) << 8) + int.Parse(m.Groups["port2"].Value);
+
+				//use host ip if server advertises a non-routeable IP
+				m = Regex.Match(host,@"(^10\.)|(^172\.1[6-9]\.)|(^172\\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)|(^127\.0\.0\.1)|(^0\.0\.0\.0)");
+
+				if(m.Success){			
+					host = m_host;
+				}
 			}
 
 			stream = new FtpDataStream(this);
@@ -234,6 +241,13 @@ namespace FluentFTP {
                     host = (m.Groups["quad1"].Value + "." + m.Groups["quad2"].Value + "." + m.Groups["quad3"].Value + "." + m.Groups["quad4"].Value);
 
                 port = (int.Parse(m.Groups["port1"].Value) << 8) + int.Parse(m.Groups["port2"].Value);
+
+								//use host ip if server advertises a non-routeable IP
+				m = Regex.Match(host,@"(^10\.)|(^172\.1[6-9]\.)|(^172\\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)|(^127\.0\.0\.1)|(^0\.0\.0\.0)");
+
+				if(m.Success){			
+					host = m_host;
+				}
             }
 
             stream = new FtpDataStream(this);
