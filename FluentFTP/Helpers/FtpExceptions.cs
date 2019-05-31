9cb974d8315862e1a947fdc7e8323931ca7760e3
Erik Wramner <erik.wramner@codemint.com>
2019-05-31 13:08:34 +0200

    #399 Use specific "auth failed" exception

--- FluentFTP/Helpers/FtpExceptions.cs
+++ FluentFTP/Helpers/FtpExceptions.cs
@@ -96,6 +96,35 @@ namespace FluentFTP {
 	}
 
 	/// <summary>
+	/// Exception triggered on authentication failures
+	/// </summary>
+#if !CORE
+	[Serializable]
+#endif
+	public class FtpAuthenticationException : FtpCommandException
+	{
+		/// <summary>
+		/// Initializes a new instance of a FtpAuthenticationException
+		/// </summary>
+		/// <param name="code">Status code</param>
+		/// <param name="message">Associated message</param>
+		public FtpAuthenticationException(string code, string message) : base(code, message) { }
+
+		/// <summary>
+		/// Initializes a new instance of a FtpAuthenticationException
+		/// </summary>
+		/// <param name="reply">The FtpReply to build the exception from</param>
+		public FtpAuthenticationException(FtpReply reply) : base(reply) { }
+
+#if !CORE
+		/// <summary>
+		/// Must be implemented so every Serializer can Deserialize the Exception
+		/// </summary>
+		protected FtpAuthenticationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+#endif
+	}
+
+	/// <summary>
 	/// Exception is thrown when encryption could not be negotiated by the server
 	/// </summary>
 #if !CORE
