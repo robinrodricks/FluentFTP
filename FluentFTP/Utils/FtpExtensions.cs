8594d522dc6ffe2357ba1153ada8d7874d803458
Robinicks <robinrodricks7@gmail.com>
2019-07-15 12:21:46 +0530

    c# string escaping

--- FluentFTP/Utils/FtpExtensions.cs
+++ FluentFTP/Utils/FtpExtensions.cs
@@ -478,5 +478,43 @@ namespace FluentFTP {
 			return (owner & flag) == flag;
 		}
 
+		/// <summary>
+		/// Escape a string into a valid C# string literal.
+		/// Implementation from StackOverflow - https://stackoverflow.com/a/14087738
+		/// </summary>
+		public static string EscapeStringLiteral(this string input) {
+			StringBuilder literal = new StringBuilder(input.Length + 2);
+			literal.Append("\"");
+			foreach (var c in input) {
+				switch (c) {
+					case '\'': literal.Append(@"\'"); break;
+					case '\"': literal.Append("\\\""); break;
+					case '\\': literal.Append(@"\\"); break;
+					case '\0': literal.Append(@"\0"); break;
+					case '\a': literal.Append(@"\a"); break;
+					case '\b': literal.Append(@"\b"); break;
+					case '\f': literal.Append(@"\f"); break;
+					case '\n': literal.Append(@"\n"); break;
+					case '\r': literal.Append(@"\r"); break;
+					case '\t': literal.Append(@"\t"); break;
+					case '\v': literal.Append(@"\v"); break;
+					default:
+
+						// ASCII printable character
+						if (c >= 0x20 && c <= 0x7e) {
+							literal.Append(c);
+						} else {
+
+							// As UTF16 escaped character
+							literal.Append(@"\u");
+							literal.Append(((int)c).ToString("x4"));
+						}
+						break;
+				}
+			}
+			literal.Append("\"");
+			return literal.ToString();
+		}
+
 	}
 }
