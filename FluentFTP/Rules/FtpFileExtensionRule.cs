097373f018a438ce87d001e81a0bc8b2003dfa5d
Robinicks <robinrodricks7@gmail.com>
2020-01-29 18:30:36 +0530

    new file extension rule

---
+++ FluentFTP/Rules/FtpFileExtensionRule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentFTP.Helpers;
+
+namespace FluentFTP {
+
+	/// <summary>
+	/// Only accept files that have the given extension, or exclude files of a given extension.
+	/// </summary>
+	public class FtpFileExtensionRule : FtpRule {
+
+		/// <summary>
+		/// If true, only files of the given extension are downloaded. If false, files of the given extension are excluded.
+		/// </summary>
+		public bool Whitelist;
+
+		/// <summary>
+		/// The extensions to match
+		/// </summary>
+		public IList<string> Exts;
+
+		/// <summary>
+		/// Only accept files that have the given extension, or exclude files of a given extension.
+		/// </summary>
+		/// <param name="whitelist">If true, only files of the given extension are downloaded. If false, files of the given extension are excluded.</param>
+		/// <param name="exts">The extensions to match</param>
+		public FtpFileExtensionRule(bool whitelist, IList<string> exts) {
+			this.Whitelist = whitelist;
+			this.Exts = exts;
+		}
+
+		/// <summary>
+		/// Checks if the files has the given extension, or exclude files of the given extension.
+		/// </summary>
+		public override bool IsAllowed(FtpListItem item) {
+			if (item.Type == FtpFileSystemObjectType.File) {
+				var ext = Path.GetExtension(item.Name);
+				if (Whitelist) {
+
+					// whitelist
+					if (ext.IsBlank()) {
+						return false;
+					}
+					else {
+						return Exts.Contains(ext);
+					}
+				}
+				else {
+
+					// blacklist
+					if (ext.IsBlank()) {
+						return true;
+					}
+					else {
+						return !Exts.Contains(ext);
+					}
+				}
+			}
+			else {
+				return true;
+			}
+		}
+
+	}
+}
