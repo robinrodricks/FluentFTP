269f571bc2a8906c8a7a639095c7a737d833cd3f
Robinicks <robinrodricks7@gmail.com>
2020-01-29 15:55:29 +0530

    new props for tracking multi file transfers

--- FluentFTP/Helpers/FtpProgress.cs
+++ FluentFTP/Helpers/FtpProgress.cs
@@ -7,18 +7,43 @@ namespace FluentFTP {
 	public class FtpProgress {
 		/// <summary>
 		/// A value between 0-100 indicating percentage complete, or -1 for indeterminate.
+		/// Used to track the progress of an individual file transfer.
 		/// </summary>
 		public double Progress { get; set; }
 
 		/// <summary>
-		/// A value representing the current Transfer Speed in Bytes per seconds
+		/// A value representing the current Transfer Speed in Bytes per seconds.
+		/// Used to track the progress of an individual file transfer.
 		/// </summary>
 		public double TransferSpeed { get; set; }
 
 		/// <summary>
-		/// A value representing the calculated 'Estimated time of arrival'
+		/// A value representing the calculated 'Estimated time of arrival'.
+		/// Used to track the progress of an individual file transfer.
 		/// </summary>
 		public TimeSpan ETA { get; set; }
+
+		/// <summary>
+		/// Stores the absolute remote path of the the current file being transfered.
+		/// </summary>
+		public string RemotePath { get; set; }
+
+		/// <summary>
+		/// Stores the absolute local path of the the current file being transfered.
+		/// </summary>
+		public string LocalPath { get; set; }
+
+		/// <summary>
+		/// Stores the index of the the file in the listing.
+		/// Only used when transfering multiple files or an entire directory.
+		/// </summary>
+		public int FileIndex { get; set; }
+
+		/// <summary>
+		/// Stores the total count of the files to be transfered.
+		/// Only used when transfering multiple files or an entire directory.
+		/// </summary>
+		public int FileCount { get; set; }
 
 		/// <summary>
 		/// Contructor for the class
