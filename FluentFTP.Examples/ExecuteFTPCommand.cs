a5f2867b247620ed76a5eb88ed25a69b8b4ac0a1
Robinicks <robinrodricks7@gmail.com>
2020-02-08 11:31:03 +0530

    add execute example

---
+++ FluentFTP.Examples/ExecuteFTPCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentFTP;
+
+namespace Examples {
+	internal static class ExecuteFTPCommandExample {
+
+		public static void Execute() {
+			using (var conn = new FtpClient("127.0.0.1", "ftptest", "ftptest")) {
+				conn.Connect();
+
+				FtpReply reply;
+				if (!(reply = conn.Execute("SITE CHMOD 640 FOO.TXT")).Success) {
+					throw new FtpCommandException(reply);
+				}
+			}
+		}
+
+		public static async Task ExecuteAsync() {
+			var token = new CancellationToken();
+			using (var conn = new FtpClient("127.0.0.1", "ftptest", "ftptest")) {
+				await conn.ConnectAsync(token);
+
+				FtpReply reply;
+				if (!(reply = await conn.ExecuteAsync("SITE CHMOD 640 FOO.TXT", token)).Success) {
+					throw new FtpCommandException(reply);
+				}
+			}
+		}
+
+
+	}
+}
