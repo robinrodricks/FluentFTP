beeef16ba2e8649b02b0eece56c61f616d418da5
Robinicks <robinrodricks7@gmail.com>
2020-02-08 11:30:52 +0530

    add proper FTPS examples

---
+++ FluentFTP.Examples/ConnectFTPS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentFTP;
+
+namespace Examples {
+	internal static class ConnectFTPSExample {
+
+		public static void ConnectFTPS() {
+			using (var conn = new FtpClient("127.0.0.1", "ftptest", "ftptest")) {
+				conn.EncryptionMode = FtpEncryptionMode.Explicit;
+				conn.ValidateAnyCertificate = true;
+				conn.Connect();
+			}
+		}
+
+		public static async Task ConnectFTPSAsync() {
+			var token = new CancellationToken();
+			using (var conn = new FtpClient("127.0.0.1", "ftptest", "ftptest")) {
+
+				conn.EncryptionMode = FtpEncryptionMode.Explicit;
+				conn.ValidateAnyCertificate = true;
+				await conn.ConnectAsync(token);
+			}
+		}
+
+
+	}
+}
---
+++ FluentFTP.Examples/ConnectFTPSCertificate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentFTP;
+
+namespace Examples {
+	internal static class ConnectFTPSCertificateExample {
+
+		public static void ConnectFTPSCertificate() {
+			using (var conn = new FtpClient("127.0.0.1", "ftptest", "ftptest")) {
+				conn.EncryptionMode = FtpEncryptionMode.Explicit;
+				conn.ValidateCertificate += new FtpSslValidation(OnValidateCertificate);
+				conn.Connect();
+			}
+		}
+
+		public static async Task ConnectFTPSCertificateAsync() {
+			var token = new CancellationToken();
+			using (var conn = new FtpClient("127.0.0.1", "ftptest", "ftptest")) {
+
+				conn.EncryptionMode = FtpEncryptionMode.Explicit;
+				conn.ValidateCertificate += new FtpSslValidation(OnValidateCertificate);
+				await conn.ConnectAsync(token);
+			}
+		}
+
+		private static void OnValidateCertificate(FtpClient control, FtpSslValidationEventArgs e) {
+			if (e.PolicyErrors != System.Net.Security.SslPolicyErrors.None) {
+				// invalid cert, do you want to accept it?
+				// e.Accept = true;
+			}
+			else {
+				e.Accept = true;
+			}
+		}
+
+	}
+}
