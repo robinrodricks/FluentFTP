19e85be93badde5848cff20b1fdfb92cec0a3d22
Robin Rodricks <robinrodricks7@gmail.com>
2020-04-25 12:05:15 +0530

    add new properties

--- FluentFTP/Client/IFtpClient.cs
+++ FluentFTP/Client/IFtpClient.cs
@@ -1,4 +1,5 @@
 using FluentFTP.Rules;
+using FluentFTP.Servers;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -23,21 +24,23 @@ namespace FluentFTP {
 	/// For detailed documentation of the methods, please see the FtpClient class or check the Wiki on the FluentFTP Github project.
 	/// </summary>
 	public interface IFtpClient : IDisposable {
-		// CONNECTION
 
-		bool IsDisposed { get; }
+		// PROPERTIES (From FtpClient_Properties)
+
+		bool IsDisposed { get; set; }
 		FtpIpVersion InternetProtocolVersions { get; set; }
 		int SocketPollInterval { get; set; }
 		bool StaleDataCheck { get; set; }
-		bool IsConnected { get; }
+		bool IsConnected { get; set; }
 		bool EnableThreadSafeDataConnections { get; set; }
 		int NoopInterval { get; set; }
+		bool CheckCapabilities { get; set; }
 		Encoding Encoding { get; set; }
 		string Host { get; set; }
 		int Port { get; set; }
 		NetworkCredential Credentials { get; set; }
 		int MaximumDereferenceCount { get; set; }
-		X509CertificateCollection ClientCertificates { get; }
+		X509CertificateCollection ClientCertificates { get; set; }
 		Func<string> AddressResolver { get; set; }
 		IEnumerable<int> ActivePorts { get; set; }
 		FtpDataConnectionType DataConnectionType { get; set; }
@@ -47,26 +50,48 @@ namespace FluentFTP {
 		int DataConnectionConnectTimeout { get; set; }
 		int DataConnectionReadTimeout { get; set; }
 		bool SocketKeepAlive { get; set; }
-		List<FtpCapability> Capabilities { get; }
-		FtpHashAlgorithm HashAlgorithms { get; }
+		List<FtpCapability> Capabilities { get; set; }
+		FtpHashAlgorithm HashAlgorithms { get; set; }
 		FtpEncryptionMode EncryptionMode { get; set; }
 		bool DataConnectionEncryption { get; set; }
+		bool PlainTextEncryption { get; set; }
 		SslProtocols SslProtocols { get; set; }
+		FtpsBuffering SslBuffering { get; set; }
+		event FtpSslValidation ValidateCertificate;
+		bool ValidateAnyCertificate { get; set; }
+		bool ValidateCertificateRevocation { get; set; }
 		string SystemType { get; }
-		string ConnectionType { get; }
+		FtpServer ServerType { get; }
+		FtpBaseServer ServerHandler { get; set; }
+		FtpOperatingSystem ServerOS { get; }
+		string ConnectionType { get; set; }
+		FtpReply LastReply { get; set; }
+		FtpDataType ListingDataType { get; set; }
 		FtpParser ListingParser { get; set; }
 		CultureInfo ListingCulture { get; set; }
-		double TimeOffset { get; set; }
 		bool RecursiveList { get; set; }
+		double TimeOffset { get; set; }
+		DateTimeStyles TimeConversion { get; set; }
 		bool BulkListing { get; set; }
 		int BulkListingLength { get; set; }
 		int TransferChunkSize { get; set; }
 		int RetryAttempts { get; set; }
 		uint UploadRateLimit { get; set; }
 		uint DownloadRateLimit { get; set; }
+		bool DownloadZeroByteFiles { get; set; }
 		FtpDataType UploadDataType { get; set; }
 		FtpDataType DownloadDataType { get; set; }
-		event FtpSslValidation ValidateCertificate;
+		bool UploadDirectoryDeleteExcluded { get; set; }
+		bool DownloadDirectoryDeleteExcluded { get; set; }
+		FtpDataType FXPDataType { get; set; }
+		int FXPProgressInterval { get; set; }
+		bool SendHost { get; set; }
+		string SendHostDomain { get; set; }
+
+
+
+		// METHODS
+
 		FtpReply Execute(string command);
 		FtpReply GetReply();
 		void Connect();
