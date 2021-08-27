2a39aaa2931bf72bd4ee622b5249ee03f36a32b9
Robin Rodricks <robinrodricks7@gmail.com>
2021-08-27 19:47:09 +0530

    neaten logging implementation

--- FluentFTP/Client/FtpClient_Utils.cs
+++ FluentFTP/Client/FtpClient_Utils.cs
@@ -26,7 +26,6 @@ using System.Threading.Tasks;
 
 namespace FluentFTP {
 	public partial class FtpClient : IDisposable {
-		#region Utils
 
 		/// <summary>
 		/// Performs a bitwise and to check if the specified
@@ -287,84 +286,6 @@ namespace FluentFTP {
 			}
 			return true;
 		}
-		
-
-		#endregion
-
-		#region Logging
-
-		/// <summary>
-		/// Add a custom listener here to get events every time a message is logged.
-		/// </summary>
-		public Action<FtpTraceLevel, string> OnLogEvent;
-
-		/// <summary>
-		/// Log a function call with relevant arguments
-		/// </summary>
-		/// <param name="function">The name of the API function</param>
-		/// <param name="args">The args passed to the function</param>
-		public void LogFunc(string function, object[] args = null) {
-			// log to attached logger if given
-			if (OnLogEvent != null) {
-				OnLogEvent(FtpTraceLevel.Verbose, ">         " + function + "(" + args.ItemsToString().Join(", ") + ")");
-			}
-
-			// log to system
-			FtpTrace.WriteFunc(function, args);
-		}
-
-		/// <summary>
-		/// Log a message
-		/// </summary>
-		/// <param name="eventType">The type of tracing event</param>
-		/// <param name="message">The message to write</param>
-		public void LogLine(FtpTraceLevel eventType, string message) {
-			// log to attached logger if given
-			if (OnLogEvent != null) {
-				OnLogEvent(eventType, message);
-			}
-
-			// log to system
-			FtpTrace.WriteLine(eventType, message);
-		}
-
-		/// <summary>
-		/// Log a message, adding an automatic prefix to the message based on the `eventType`
-		/// </summary>
-		/// <param name="eventType">The type of tracing event</param>
-		/// <param name="message">The message to write</param>
-		public void LogStatus(FtpTraceLevel eventType, string message) {
-			// add prefix
-			message = TraceLevelPrefix(eventType) + message;
-
-			// log to attached logger if given
-			if (OnLogEvent != null) {
-				OnLogEvent(eventType, message);
-			}
-
-			// log to system
-			FtpTrace.WriteLine(eventType, message);
-		}
-
-		private static string TraceLevelPrefix(FtpTraceLevel level) {
-			switch (level) {
-				case FtpTraceLevel.Verbose:
-					return "Status:   ";
-
-				case FtpTraceLevel.Info:
-					return "Status:   ";
-
-				case FtpTraceLevel.Warn:
-					return "Warning:  ";
-
-				case FtpTraceLevel.Error:
-					return "Error:    ";
-			}
-
-			return "Status:   ";
-		}
-
-		#endregion
 
 	}
 }
