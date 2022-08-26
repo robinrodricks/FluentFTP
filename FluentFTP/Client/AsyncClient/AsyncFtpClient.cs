62b5cdd9e5b668bee1c1f2aadfc3550d4acc6182
Robin Rodricks <robinrodricks7@gmail.com>
2022-08-26 20:29:40 +0530

    cleanup

--- FluentFTP/Client/AsyncClient/AsyncFtpClient.cs
+++ FluentFTP/Client/AsyncClient/AsyncFtpClient.cs
@@ -1,22 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Net.Sockets;
-using System.Text;
-using System.Reflection;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Net;
-using FluentFTP.Proxy;
-using SysSslProtocols = System.Security.Authentication.SslProtocols;
-using FluentFTP.Helpers;
-#if !NETSTANDARD
-using System.Web;
-#endif
-using System.Threading;
-using System.Threading.Tasks;
-using FluentFTP.Client.Modules;
-using FluentFTP;
 using FluentFTP.Client.BaseClient;
 
 namespace FluentFTP {
