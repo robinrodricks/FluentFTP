d688aa685661440ed447d53b57f442a0c4a76f91
FanDjango <FanDjango@ansynova.eu>
2022-09-23 19:48:38 +0200

    Add Test

---
+++ FluentFTP.Tests/Unit/ExceptionTests.cs
@@ -0,0 +1,20 @@
+using FluentFTP.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+
+namespace FluentFTP.Tests.Unit {
+	public class ExceptionTests {
+
+		[Fact]
+		public void FtpCommandException_includes_CompletionCode_in_message() {
+
+			Action act = () => throw new FtpCommandException("501", "MyErrorMessage");
+
+			var exception = Assert.Throws<FtpCommandException>(act);
+			Assert.Contains("501", exception.ToString());
+			Assert.Contains("MyErrorMessage", exception.ToString());
+		}
+	}
+}
