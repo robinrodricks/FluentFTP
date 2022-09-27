cbf7dcfd33f6d1dd3fddcbd2d6e83a73abbfca13
FanDjango <51046875+FanDjango@users.noreply.github.com>
2022-09-27 11:08:32 +0200

    Modify the calls to ReadStaleData

--- FluentFTP/Client/SyncClient/UploadFileInternal.cs
+++ FluentFTP/Client/SyncClient/UploadFileInternal.cs
@@ -253,7 +253,7 @@ namespace FluentFTP {
 
 						// Fix #387: exhaust any NOOP responses also after "226 Transfer complete."
 						if (anyNoop) {
-							ReadStaleData(false, true, true);
+							ReadStaleData(false, true, "after upload");
 						}
 
 						break;
