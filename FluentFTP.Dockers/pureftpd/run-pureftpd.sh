7637aee085d463385d2b812ebbe8483997bccf13
FanDjango <FanDjango@ansynova.eu>
2022-10-23 22:38:32 +0200

    Part 1: Docker folder

---
+++ FluentFTP.Dockers/pureftpd/run-pureftpd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# stdout server info:
+cat << EOB
+	*************************************************
+	*                                               *
+	*    Docker image: fluentftp pureftpd           *
+	*                                               *
+	*************************************************
+
+	SERVER SETTINGS
+	---------------
+	· FTP User: fluentuser
+	· FTP Password: fluentpass
+EOB
+
+if [[ -n "${USE_SSL}" ]]; then
+  TLS=1
+else
+  TLS=0
+fi
+
+# Run pureftpd:              
+&>/dev/null /usr/sbin/pure-ftpd -A -E -j -R -l unix -p 21100:21199 --tls $TLS
