d77b2210a16522b3b7183f636b0d7795dbeac5ef
FanDjango <51046875+FanDjango@users.noreply.github.com>
2022-11-25 14:22:51 +0100

    Add docker container for Apache FTP Server

--- FluentFTP.Dockers/Build.bat
+++ FluentFTP.Dockers/Build.bat
@@ -1,16 +1,17 @@
+docker build apache -t apache:fluentftp
+
+docker build bftpd -t bftpd:fluentftp
+
+docker build filezilla -t filezilla:fluentftp
+
+docker build glftpd -t glftpd:fluentftp
 
 docker build proftpd -t proftpd:fluentftp
 
 docker build pureftpd -t pureftpd:fluentftp
 
-docker build vsftpd -t vsftpd:fluentftp
-
 docker build pyftpdlib -t pyftpdlib:fluentftp
 
-docker build bftpd -t bftpd:fluentftp
-
-docker build glftpd -t glftpd:fluentftp
-
-docker build filezilla -t filezilla:fluentftp
+docker build vsftpd -t vsftpd:fluentftp
 
 pause
