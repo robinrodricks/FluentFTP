2a9eae7729de7262ca07e3241fa150727b85f4af
FanDjango <51046875+FanDjango@users.noreply.github.com>
2022-11-27 12:25:45 +0100

    Improve Docker Build by attempting to find better mirror and by using per-built utility images.

--- FluentFTP.Dockers/Build.bat
+++ FluentFTP.Dockers/Build.bat
@@ -1,17 +1,68 @@
-docker build apache -t apache:fluentftp
+rem uncomment for brutal image cleanup, force total rebuild
+rem docker builder prune --all -f
 
-docker build bftpd -t bftpd:fluentftp
+rem can use --pull, --no-cache --progress=plain
 
-docker build filezilla -t filezilla:fluentftp
+rem
+rem Debian mirror selection using a helper container
+rem
+docker build common-mirror --pull --progress=plain -t common-mirror:fluentftp
+docker run --name mirror common-mirror:fluentftp
+docker cp mirror:/root/sources.list sources.list
+docker rm mirror
 
-docker build glftpd -t glftpd:fluentftp
+rem
+rem distribute the sources.list to the dockers for speed
+rem
+copy sources.list common-debian
+copy sources.list common-debian-slim
+copy sources.list apache
+copy sources.list bftpd
+copy sources.list filezilla
+copy sources.list glftpd
+copy sources.list proftpd
+copy sources.list pureftpd
+rem for pyftpdlib, not needed
+copy sources.list vsftpd
 
-docker build proftpd -t proftpd:fluentftp
+rem
+rem build the common-debian build environment
+rem
+docker build common-debian --pull --progress=plain -t common-debian:fluentftp
 
-docker build pureftpd -t pureftpd:fluentftp
+rem
+rem build the common-debian production environment
+rem
+docker build common-debian-slim --pull --progress=plain -t common-debian-slim:fluentftp
 
-docker build pyftpdlib -t pyftpdlib:fluentftp
+rem
+rem build the actual server images
+rem
+docker build apache    --progress=plain -t apache:fluentftp
+docker build bftpd     --progress=plain -t bftpd:fluentftp
+docker build filezilla --progress=plain -t filezilla:fluentftp
+docker build glftpd    --progress=plain -t glftpd:fluentftp
+docker build proftpd   --progress=plain -t proftpd:fluentftp
+docker build pureftpd  --progress=plain -t pureftpd:fluentftp
+docker build pyftpdlib --progress=plain -t pyftpdlib:fluentftp
+docker build vsftpd    --progress=plain -t vsftpd:fluentftp
 
-docker build vsftpd -t vsftpd:fluentftp
+rem
+rem clean up
+rem
+del sources.list
+del common-debian\sources.list
+del common-debian-slim\sources.list
+del apache\sources.list
+del bftpd\sources.list
+del filezilla\sources.list
+del glftpd\sources.list
+del proftpd\sources.list
+del pureftpd\sources.list
+rem for pyftpdlib, not needed
+del vsftpd\sources.list
+
+rem uncomment for brutal image cleanup, force total rebuild
+rem docker image prune -f
 
 pause
