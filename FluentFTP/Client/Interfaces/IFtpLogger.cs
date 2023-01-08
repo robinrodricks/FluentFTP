93cde9c0061e7300da8126237af7e6ac05580adf
FanDjango <51046875+FanDjango@users.noreply.github.com>
2023-01-08 11:56:48 +0100

    Cleanup some missing XML warnings

--- FluentFTP/Client/Interfaces/IFtpLogger.cs
+++ FluentFTP/Client/Interfaces/IFtpLogger.cs
@@ -1,5 +1,11 @@
 ﻿namespace FluentFTP {
+
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
 	public interface IFtpLogger {
 		void Log(FtpLogEntry entry);
 	}
+
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+
 }
