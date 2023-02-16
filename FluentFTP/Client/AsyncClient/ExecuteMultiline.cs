a2d2d16c5ee5b6a417b1fcab593ee1f48673b8af
Robin Rodricks <robinrodricks7@gmail.com>
2023-02-16 17:31:07 +0530

    rename to ExecuteMultiline

--- FluentFTP/Client/AsyncClient/ExecuteMultiline.cs
+++ FluentFTP/Client/AsyncClient/ExecuteMultiline.cs
@@ -9,46 +9,49 @@ using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 using FluentFTP.Client.Modules;
 using System.Security.Authentication;
-using FluentFTP.Proxy.SyncProxy;
+using FluentFTP.Proxy.AsyncProxy;
 
 namespace FluentFTP {
-	public partial class FtpClient {
-
+	public partial class AsyncFtpClient {
 
 		/// <summary>
-		/// Gets command output from the server. 
+		/// Execute a custom FTP command and return its multiline output. If you only need a single line output, use `Execute` instead.
 		/// </summary>
-		/// </remarks>
 		/// <param name="command">The command to issue which produces output</param>
-		/// <returns>An list of string objects</returns>
-		public List<string> GetCommandOutput(string command) {
-			return GetCommandOutputInternal(command, true);
+		/// <returns>A list of string objects corresponding to the multi-line response by the server</returns>
+		public async Task<List<string>> ExecuteMultiline(string command, CancellationToken token = default(CancellationToken)) {
+
+			return await ExecuteMultilineInternal(command, true, token);
+
 		}
 
 		/// <summary>
-		/// Get the records of a command output and retry if temporary failure.
+		/// Execute a custom FTP command and return its multiline output. If you only need a single line output, use `Execute` instead.
 		/// </summary>
-		protected List<string> GetCommandOutputInternal(string command, bool retry) {
+		/// <param name="command">The command to issue which produces output</param>
+		/// <param name="retry">Retry the command execution on temporary failure?</param>
+		/// <returns>A list of string objects corresponding to the multi-line response by the server</returns>
+		protected async Task<List<string>> ExecuteMultilineInternal(string command, bool retry, CancellationToken token) {
 
 			List<string> rawlisting = new List<string> { "Lines captured:" };
 
 			try {
 				// read in raw command output from data stream
 				try {
-					using (var stream = OpenDataStream(command, 0)) {
+					using (FtpDataStream stream = await OpenDataStreamAsync(command, 0, token)) {
 						try {
-							if (this is FtpClientSocks4Proxy || this is FtpClientSocks4aProxy) {
+							if (this is AsyncFtpClientSocks4Proxy || this is AsyncFtpClientSocks4aProxy) {
 								// first 6 bytes contains 2 bytes of unknown (to me) purpose and 4 ip address bytes
 								// we need to skip them otherwise they will be downloaded to the file
 								// moreover, these bytes cause "Failed to get the EPSV port" error
-								stream.Read(new byte[6], 0, 6);
+								await stream.ReadAsync(new byte[6], 0, 6);
 							}
 
 							Log(FtpTraceLevel.Verbose, "+---------------------------------------+");
 
 							if (Config.BulkListing) {
 								// increases performance of GetListing by reading multiple lines of the command output at once
-								foreach (var line in stream.ReadAllLines(Encoding, Config.BulkListingLength)) {
+								foreach (var line in await stream.ReadAllLinesAsync(Encoding, Config.BulkListingLength, token)) {
 									if (!Strings.IsNullOrWhiteSpace(line)) {
 										rawlisting.Add(line);
 										Log(FtpTraceLevel.Verbose, "Lines  :  " + line);
@@ -58,7 +61,7 @@ namespace FluentFTP {
 							else {
 								// Read command output line-by-line (actually byte-by-byte)
 								string buf;
-								while ((buf = stream.ReadLine(Encoding)) != null) {
+								while ((buf = await stream.ReadLineAsync(Encoding, token)) != null) {
 									if (buf.Length > 0) {
 										rawlisting.Add(buf);
 										Log(FtpTraceLevel.Verbose, "Lines  :  " + buf);
@@ -76,7 +79,7 @@ namespace FluentFTP {
 					}
 				}
 				catch (AuthenticationException) {
-					FtpReply reply = GetReplyInternal("*GETCOMMANDOUTPUT*", false, -1); // no exhaustNoop, but non-blocking
+					FtpReply reply = await GetReplyAsyncInternal(token, "*GETCOMMANDOUTPUT*", false, -1); // no exhaustNoop, but non-blocking
 					if (!reply.Success) {
 						throw new FtpCommandException(reply);
 					}
@@ -104,8 +107,8 @@ namespace FluentFTP {
 					if (retry) {
 						// retry once more, but do not go into a infinite recursion loop here
 						// note: this will cause an automatic reconnect in Execute(...)
-						Log(FtpTraceLevel.Verbose, "Warning:  Retry GetCommandOutput once more due to control connection disconnect");
-						return GetCommandOutputInternal(command, false);
+						Log(FtpTraceLevel.Verbose, "Warning:  Retry ExecuteMultiline once more due to control connection disconnect");
+						return await ExecuteMultilineInternal(command, false, token);
 					}
 					else {
 						throw;
@@ -115,8 +118,8 @@ namespace FluentFTP {
 				// Fix #410: Retry if its a temporary failure ("Received an unexpected EOF or 0 bytes from the transport stream")
 				if (retry && ioEx.Message.ContainsAnyCI(ServerStringModule.unexpectedEOF)) {
 					// retry once more, but do not go into a infinite recursion loop here
-					Log(FtpTraceLevel.Verbose, "Warning:  Retry GetCommandOutput once more due to unexpected EOF");
-					return GetCommandOutputInternal(command, false);
+					Log(FtpTraceLevel.Verbose, "Warning:  Retry ExecuteMultiline once more due to unexpected EOF");
+					return await ExecuteMultilineInternal(command, false, token);
 				}
 				else {
 					// suppress all other types of exceptions
@@ -125,6 +128,5 @@ namespace FluentFTP {
 
 			return rawlisting;
 		}
-
 	}
 }
---
+++ FluentFTP/Client/SyncClient/ExecuteMultiline.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using FluentFTP.Exceptions;
+using FluentFTP.Helpers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using FluentFTP.Client.Modules;
+using System.Security.Authentication;
+using FluentFTP.Proxy.SyncProxy;
+
+namespace FluentFTP {
+	public partial class FtpClient {
+
+
+		/// <summary>
+		/// Execute a custom FTP command and return its multiline output. If you only need a single line output, use `Execute` instead.
+		/// </summary>
+		/// <param name="command">The command to issue which produces output</param>
+		/// <param name="retry">Retry the command execution on temporary failure?</param>
+		/// <returns>A list of string objects corresponding to the multi-line response by the server</returns>
+		protected List<string> ExecuteMultiline(string command, bool retry = true) {
+
+			List<string> rawlisting = new List<string> { "Lines captured:" };
+
+			try {
+				// read in raw command output from data stream
+				try {
+					using (var stream = OpenDataStream(command, 0)) {
+						try {
+							if (this is FtpClientSocks4Proxy || this is FtpClientSocks4aProxy) {
+								// first 6 bytes contains 2 bytes of unknown (to me) purpose and 4 ip address bytes
+								// we need to skip them otherwise they will be downloaded to the file
+								// moreover, these bytes cause "Failed to get the EPSV port" error
+								stream.Read(new byte[6], 0, 6);
+							}
+
+							Log(FtpTraceLevel.Verbose, "+---------------------------------------+");
+
+							if (Config.BulkListing) {
+								// increases performance of GetListing by reading multiple lines of the command output at once
+								foreach (var line in stream.ReadAllLines(Encoding, Config.BulkListingLength)) {
+									if (!Strings.IsNullOrWhiteSpace(line)) {
+										rawlisting.Add(line);
+										Log(FtpTraceLevel.Verbose, "Lines  :  " + line);
+									}
+								}
+							}
+							else {
+								// Read command output line-by-line (actually byte-by-byte)
+								string buf;
+								while ((buf = stream.ReadLine(Encoding)) != null) {
+									if (buf.Length > 0) {
+										rawlisting.Add(buf);
+										Log(FtpTraceLevel.Verbose, "Lines  :  " + buf);
+									}
+								}
+							}
+
+							Log(FtpTraceLevel.Verbose, "-----------------------------------------");
+						}
+						finally {
+							// We want to close/dispose it NOW, and not when the GM
+							// gets around to it (after the "using" expires).
+							stream.Close();
+						}
+					}
+				}
+				catch (AuthenticationException) {
+					FtpReply reply = GetReplyInternal("*GETCOMMANDOUTPUT*", false, -1); // no exhaustNoop, but non-blocking
+					if (!reply.Success) {
+						throw new FtpCommandException(reply);
+					}
+					throw;
+				}
+			}
+			catch (FtpMissingSocketException) {
+				// Some FTP server does not send any response when producing an empty response
+				// and the connection fails because no communication socket is provided by the server
+			}
+			catch (FtpCommandException ftpEx) {
+				// Fix for #589 - CompletionCode is null
+				if (ftpEx.CompletionCode == null) {
+					throw new FtpException(ftpEx.Message, ftpEx);
+				}
+				// Some FTP servers throw 550 for empty folders. Absorb these.
+				if (!ftpEx.CompletionCode.StartsWith("550")) {
+					throw ftpEx;
+				}
+			}
+			catch (IOException ioEx) {
+				// Some FTP servers forcibly close the connection, we absorb these errors,
+				// unless we have lost the control connection itself
+				if (m_stream.IsConnected == false) {
+					if (retry) {
+						// retry once more, but do not go into a infinite recursion loop here
+						// note: this will cause an automatic reconnect in Execute(...)
+						Log(FtpTraceLevel.Verbose, "Warning:  Retry ExecuteMultiline once more due to control connection disconnect");
+						return ExecuteMultiline(command, false);
+					}
+					else {
+						throw;
+					}
+				}
+
+				// Fix #410: Retry if its a temporary failure ("Received an unexpected EOF or 0 bytes from the transport stream")
+				if (retry && ioEx.Message.ContainsAnyCI(ServerStringModule.unexpectedEOF)) {
+					// retry once more, but do not go into a infinite recursion loop here
+					Log(FtpTraceLevel.Verbose, "Warning:  Retry ExecuteMultiline once more due to unexpected EOF");
+					return ExecuteMultiline(command, false);
+				}
+				else {
+					// suppress all other types of exceptions
+				}
+			}
+
+			return rawlisting;
+		}
+
+	}
+}
