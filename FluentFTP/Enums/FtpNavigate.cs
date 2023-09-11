c804ae246f0c16c4dbc8a38eb116a096e6985960
FanDjango <51046875+FanDjango@users.noreply.github.com>
2023-09-11 21:11:06 +0200

    Some fixups to review

--- FluentFTP/Enums/FtpNavigate.cs
+++ FluentFTP/Enums/FtpNavigate.cs
@@ -4,7 +4,8 @@ namespace FluentFTP {
 	/// <summary>
 	/// Directory navigation mode that control how server-side directory traversal is performed.
 	/// Manual mode is the legacy version which allows users full control of the working directory.
-	/// All the other modes are smarter automatic versions where FluentFTP will take control of the working directory.
+	/// All the other modes are smarter automatic versions where FluentFTP will take control of the working directory
+	/// when executing FTP subcommands that accept a pathname[+filename] combination.
 	/// </summary>
 	[Flags]
 	public enum FtpNavigate:uint {
@@ -13,15 +14,15 @@ namespace FluentFTP {
 		/// The legacy navigation mode.
 		/// No automatic directory navigation performed by FluentFTP.
 		/// Users can `SetWorkingDirectory` (CWD).
-		/// Paths provided to FTP API can be absolute or relative to the working directory.
+		/// Paths provided to FTP API can be absolute or relative to the current working directory.
 		/// </summary>
 		Manual = 0,
 
 		/// <summary>
 		/// Fully automatic directory traversal on the server-side.
+		/// Users CAN `SetWorkingDirectory` (CWD), which will only temporarily override the CWD that will be set internally.
+		/// Paths provided to FTP API can be absolute or relative to the current working directory.
 		/// FluentFTP will automatically change the working directory based on the file path provided to the API method.
-		/// We assume that all file paths provided will be absolute paths.
-		/// Users CANNOT `SetWorkingDirectory` (CWD).
 		/// Fast mode.
 		/// </summary>
 		Auto = 1,
@@ -29,14 +30,15 @@ namespace FluentFTP {
 		/// <summary>
 		/// Fully automatic directory traversal on the server-side.
 		/// FluentFTP will automatically change the working directory based on the file path provided to the API method.
-		/// We assume that all file paths provided will be absolute paths.
-		/// Users CAN `SetWorkingDirectory` (CWD), which will override the path set internally.
-		/// This mode is slower than `Auto` because we need to restore the directory to the one the user last set after each API call.
+		/// Users CAN `SetWorkingDirectory` (CWD), which will only temporarily override the CWD that will be set internally.
+		/// Paths provided to FTP API can be absolute or relative to the current working directory.
+		/// This mode is slower than `Auto` because we will restore the directory (if needed) to the one the one that was active
+		/// prior to the API call.
 		/// </summary>
 		SemiAuto = 2,
 
 		/// <summary>
-		/// Adds a flag to the enum that allows for automatic directory traversal ONLY if the path contains spaces.
+		/// Adds a flag to the enum that allows automatic directory traversal ONLY if the path contains spaces.
 		/// Only works with `Auto` or `SemiAuto` modes.
 		/// </summary>
 		Conditional = 255,
