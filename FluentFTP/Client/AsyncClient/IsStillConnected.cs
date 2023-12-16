25549291e0e03284f00a1769c61b99f88b2ba9b8
Robin Rodricks <robinrodricks7@gmail.com>
2023-12-16 11:32:43 +0530

    new IsStillConnected API

---
+++ FluentFTP/Client/AsyncClient/IsStillConnected.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentFTP.Helpers;
+
+namespace FluentFTP {
+	public partial class FtpClient {
+
+		/// <summary>
+		/// Performs a series of tests to check if we are still connected to the FTP server.
+		/// More thourough than IsConnected.
+		/// </summary>
+		public async Task<bool> IsStillConnected() {
+			if (IsConnected && IsAuthenticated) {
+				if (Noop()) {
+					if (GetReply().Success) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+	}
+}
