9058a0a137d851f6fabe678146c31a58964bdd90
FanDjango <51046875+FanDjango@users.noreply.github.com>
2023-12-29 15:50:24 +0100

    Improve Execute.cs code for semaphore and NoopTestConnectivity

--- FluentFTP/Client/AsyncClient/Execute.cs
+++ FluentFTP/Client/AsyncClient/Execute.cs
@@ -20,8 +20,11 @@ namespace FluentFTP {
 			bool reconnect = false;
 			string reconnectReason = string.Empty;
 
+			m_sema.Wait();
+			m_sema.Release();
+
 			// Automatic reconnect because we lost the control channel?
-			if (!IsConnected) {
+			if (!IsConnected || (Config.NoopTestConnectivity && IsAuthenticated && !await IsStillConnected())) {
 				if (command == "QUIT") {
 					LogWithPrefix(FtpTraceLevel.Info, "Not sending QUIT because the connection has already been closed.");
 					return new FtpReply() {
@@ -33,26 +36,11 @@ namespace FluentFTP {
 				reconnect = true;
 				reconnectReason = "disconnected";
 			}
-
-			if (Config.NoopTestConnectivity && IsAuthenticated && !await IsStillConnected()) {
-				if (command == "QUIT") {
-					LogWithPrefix(FtpTraceLevel.Info, "Not sending QUIT because the connection has already been closed.");
-					return new FtpReply() {
-						Code = "200",
-						Message = "Connection already closed."
-					};
-				}
-
-				reconnect = true;
-				reconnectReason = "disconnected";
-			}
-
 			// Automatic reconnect on reaching SslSessionLength?
 			else if (m_stream.IsEncrypted && Config.SslSessionLength > 0 && !Status.InCriticalSequence && m_stream.SslSessionLength > Config.SslSessionLength) {
 				reconnect = true;
 				reconnectReason = "max SslSessionLength reached on";
 			}
-
 			// Check for stale data on the socket?
 			else if (Config.StaleDataCheck && Status.AllowCheckStaleData) {
 #if NETSTANDARD || NET5_0_OR_GREATER
