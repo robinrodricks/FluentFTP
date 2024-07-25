8373d5e0eeab5899921de1ef9709e54734de8fdd
FanDjango <51046875+FanDjango@users.noreply.github.com>
2024-07-25 14:16:30 +0200

    Correct some XML text

--- FluentFTP/Enums/FtpRealConnectionsStates.cs
+++ FluentFTP/Enums/FtpRealConnectionsStates.cs
@@ -7,12 +7,12 @@ namespace FluentFTP {
 	[Flags]
 	public enum FtpRealConnectionStates {
 		/// <summary>
-		/// Deamon will determine the state
+		/// Deamon will determine the state in a short while
 		/// </summary>
 		Unknown,
 
 		/// <summary>
-		/// Not good state and it will be brought down, closed, disposed
+		/// Not a good state and it will be brought down, closed and disposed soon
 		/// </summary>
 		PendingDown,
 
@@ -22,10 +22,7 @@ namespace FluentFTP {
 		Down,
 
 		/// <summary>
-		/// Connected, at least the last time the NOOP daemon checked the connection, or
-		/// the POLL daemon checked the connection, it was ok.
-		/// The POLL daemon checks control and data connections, the NOOP daemon only checks
-		/// control connections.
+		/// Connected, at least the last time the NOOP daemon checked the connection
 		/// </summary>
 		Up,
 
