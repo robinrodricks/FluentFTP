045f3661ef93d7c17d1873de72c17af2648ecb94
FanDjango <51046875+FanDjango@users.noreply.github.com>
2024-10-24 20:19:37 +0200

    Use extension for Skipped enum value

--- FluentFTP/Client/SyncClient/DownloadFiles.cs
+++ FluentFTP/Client/SyncClient/DownloadFiles.cs
@@ -73,7 +73,7 @@ namespace FluentFTP {
 					
 					// mark that the file succeeded
 					result.IsSuccess = ok.IsSuccess();
-					result.IsSkipped = ok == FtpStatus.Skipped;
+					result.IsSkipped = ok.IsSkipped();
 					result.IsFailed = ok.IsFailure();
 
 					if (ok.IsSuccess()) {
--- FluentFTP/Client/SyncClient/UploadFiles.cs
+++ FluentFTP/Client/SyncClient/UploadFiles.cs
@@ -89,7 +89,7 @@ namespace FluentFTP {
 					
 					// mark that the file succeeded
 					result.IsSuccess = ok.IsSuccess();
-					result.IsSkipped = ok == FtpStatus.Skipped;
+					result.IsSkipped = ok.IsSkipped();
 					result.IsFailed = ok.IsFailure();
 
 					if (ok.IsSuccess()) {
