23f7a7ec991e3d90c5c276226d1fc0dbe3ae587e
Tsvetko Hadzhitsenev <tsvetko.hadzhitsenev@progress.com>
2025-02-18 19:34:44 +0200

    Added SwitchToClearText.

---
+++ FluentFTP/Client/AsyncClient/SwitchToClearText.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace FluentFTP {
+	public partial class AsyncFtpClient {
+
+		/// <summary>
+		/// Send a 'CCC' comamnd to the FTP server and if successful closes the undelying ssl stream.
+		/// </summary>
+		public async Task SwitchToClearText() {
+
+			var reply = await Execute("CCC");
+			if (reply.Success) {
+				m_stream.SwitchToUnsecuredMode();
+				Config.EncryptionMode = FtpEncryptionMode.None;
+			}
+		}
+
+	}
+}
