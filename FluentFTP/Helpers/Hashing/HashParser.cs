8ba8eee17967bd65cf44b15f14adb0f9f0c9aff8
FanDjango <51046875+FanDjango@users.noreply.github.com>
2025-07-06 20:56:58 +0200

    Fix #1765

--- FluentFTP/Helpers/Hashing/HashParser.cs
+++ FluentFTP/Helpers/Hashing/HashParser.cs
@@ -14,29 +14,15 @@ namespace FluentFTP.Helpers.Hashing {
 
 			var hash = new FtpHash();
 
-			// Current draft says the server should return this:
-			//		<algorithm> <bytestart>-<byteend> <hash> <filename>
-
-			// Note: filename might contain blanks.
-
-			// Some servers respond with differing formats:
-
-			//		<algorithm> <hash>							- some FileZilla versions)
-			//		<algorithm> <bytestart>-<byteend> <hash>    - BrickFTP (files.com / Exavault.com)
-
-			// Try to parse these differing formats:
-
 			Match m;
-			if (!(m = Regex.Match(reply, @"^(?<algorithm>\S+)\s(?<bytestart>[0-9]+)-(?<byteend>[0-9]+)\s(?<hash>\S+)\s*(?<filename>.*)$")).Success) {
-				m = Regex.Match(reply, @"(?<algorithm>.+)\s(?<hash>.+)\s");
-			}
+
+			//  <Hash Method Name> [Optional Range] <Hash Code> [Optional File Name/Path]
+
+			m = Regex.Match(reply, @"^(?<algorithm>\S+)(?: \d+-\d+)? (?<hash>\S+)");
 
 			if (m != null && m.Success) {
 				hash.Algorithm = HashAlgorithms.FromString(m.Groups["algorithm"].Value);
 				hash.Value = m.Groups["hash"].Value;
-			}
-			else {
-				// failed to parse
 			}
 
 			return hash;
