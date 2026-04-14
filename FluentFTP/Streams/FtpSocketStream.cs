57a55636cafddd9b7de0f532606ee725dafa0469
FanDjango <51046875+FanDjango@users.noreply.github.com>
2026-04-14 14:54:24 +0200

    correct using/try pattern

--- FluentFTP/Streams/FtpSocketStream.cs
+++ FluentFTP/Streams/FtpSocketStream.cs
@@ -1155,8 +1155,8 @@ namespace FluentFTP {
 		/// <param name="port">The port to connect to</param>
 		/// <param name="token">The token that can be used to cancel the entire process</param>
 		private async Task<bool> ConnectHelperAsync(IPAddress ipad, int port, CancellationToken token) {
-			try {
-				using (var timeoutSrc = CancellationTokenSource.CreateLinkedTokenSource(token)) {
+			using (var timeoutSrc = CancellationTokenSource.CreateLinkedTokenSource(token)) {
+				try {
 					timeoutSrc.CancelAfter(ConnectTimeout);
 #if NET462
 					var connectResult = m_socket.BeginConnect(ipad, port, null, null);
@@ -1165,18 +1165,18 @@ namespace FluentFTP {
 					await EnableCancellation(m_socket.ConnectAsync(ipad, port), timeoutSrc.Token, () => DisposeSocket());
 #endif
 				}
-			}
 #if !NETSTANDARD
-			catch (ObjectDisposedException) {
-				throw new TimeoutException("Timed out trying to connect!");
-			}
+				catch (ObjectDisposedException) {
+					throw new TimeoutException("Timed out trying to connect!");
+				}
 #endif
-			catch (SocketException ex) when (ex.SocketErrorCode is SocketError.OperationAborted or SocketError.TimedOut) {
-				token.ThrowIfCancellationRequested();
-				throw new TimeoutException("Timed out trying to connect!");
-			}
+				catch (SocketException ex) when (ex.SocketErrorCode is SocketError.OperationAborted or SocketError.TimedOut) {
+					token.ThrowIfCancellationRequested();
+					throw new TimeoutException("Timed out trying to connect!");
+				}
 
-			return m_socket.Connected;
+				return m_socket.Connected;
+			}
 		}
 
 		/// <summary>
