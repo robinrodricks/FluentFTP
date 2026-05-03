b8b8cb2cfddc07f1388ae3847190e07a43e1e6b1
Robin Rodricks <robinrodricks7@gmail.com>
2026-05-04 00:54:59 +0530

    add encoding tests

--- FluentFTP.Tests/Unit/HelperTests.cs
+++ FluentFTP.Tests/Unit/HelperTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentFTP.Helpers;
 using FluentFTP.Model.Functions;
+using System.Text;
 using Xunit;
 
 namespace FluentFTP.Tests.Unit {
@@ -12,6 +13,27 @@ namespace FluentFTP.Tests.Unit {
 			var txt = ValuePrinter.ObjectToString(obj);
 
 			Assert.Equal("CloneConnection = True, FirstOnly = True, IncludeImplicit = True, AbortOnTimeout = True, RequireEncryption = False, ProtocolPriority = [Tls12]", txt);
+		}
+
+		[Fact]
+		public void EncodingToCode_UTF8() {
+			var enc = Encoding.UTF8;
+			var result = Encodings.ToCode(enc);
+			Assert.Equal("System.Text.Encoding.UTF8", result);
+		}
+
+		[Fact]
+		public void EncodingToCode_ASCII() {
+			var enc = Encoding.ASCII;
+			var result = Encodings.ToCode(enc);
+			Assert.Equal("System.Text.Encoding.ASCII", result);
+		}
+
+		[Fact]
+		public void EncodingToCode_GetEncoding() {
+			var enc = Encoding.GetEncoding("iso-8859-1");
+			var result = Encodings.ToCode(enc);
+			Assert.Equal("System.Text.Encoding.GetEncoding(\"iso-8859-1\")", result);
 		}
 
 	}
