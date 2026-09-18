17768a22c8e44c64e96e59069bcf761c47682c83
FanDjango <51046875+FanDjango@users.noreply.github.com>
2026-09-18 22:59:10 +0200

    Final

--- FluentFTP.Tests/Integration/IntegrationTests_BouncyCastle.cs
+++ FluentFTP.Tests/Integration/IntegrationTests_BouncyCastle.cs
@@ -9,35 +9,5 @@ namespace FluentFTP.Tests.Integration {
 
 		protected override UseStream Stream => UseStream.BouncyCastleStream;
 
-		// These can only do FTPS
-
-		// Works, but needs some TLC, runs very slowly - each BouncyCastle BYE hangs for while,
-		// does not happen on a real FileZilla Server
-		//[Fact]
-		//public async Task FileZillaSsl() {
-		//	await IntegrationTestRunner.Run(FtpServer.FileZilla, UseStream.BouncyCastleStream);
-		//}
-		// Works, but needs some TLC. Image does not always start reliably, hangs
-		//[Fact]
-		//public async Task GlftpdSsl() {
-		//	await IntegrationTestRunner.Run(FtpServer.glFTPd, UseStream.BouncyCastleStream);
-		//}
-
-		// These can only do FTP
-		[Fact]
-		public async Task Apache() {
-			await IntegrationTestRunner.Run(FtpServer.Apache, UseStream.BouncyCastleStream);
-		}
-		[Fact]
-		public async Task Bftpd() {
-			await IntegrationTestRunner.Run(FtpServer.BFTPd, UseStream.BouncyCastleStream);
-		}
-
-		// Still need SSL variants of these
-		[Fact]
-		public async Task PyFtpdLib() {
-			await IntegrationTestRunner.Run(FtpServer.PyFtpdLib, UseStream.BouncyCastleStream);
-		}
-
 	}
 }
